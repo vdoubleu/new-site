926c665afd9f1dfd50f030aff63dd5557ee684b0
Victor Wang <victorwang2001@gmail.com>
2022-02-22 00:25:09 -0500

    stuff

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,84 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Victor Wang</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+     
+      <main className="h-screen snap-y snap-proximity overflow-y-scroll h-screen">
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="h-screen flex items-center relative snap-start">
+          <div className="triangle top-0 absolute"></div>
+          <div className="flex flex-wrap justify-center w-full ">
+            <div className="m-5 rounded-full overflow-hidden">
+              <Image className="" src="/images/headpic3.JPG" height={400} width={400} alt="headshot" />
+            </div>
+            
+            <div className="m-2 flex justify-center flex-col">
+              <h1 className="text-8xl text-center md:text-left"> Hello </h1>
+              <h2 className="text-4xl text-center md:text-left"> Welcome to the future </h2>
+            </div>
+          </div>
+          <div className="rotate-180 right-0 triangle absolute bottom-0"></div>
+        </div>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className="min-h-[100vh] relative snap-start">
+          <div className="rotate-90 float-right triangle absolute right-0"></div>
+          <div className="px-2 md:px-32">
+            <div className="mb-10 pt-10 md:pt-28"> 
+              <h4 className="text-center text-4xl mb-4"> Hey! I'm Victor. </h4>
+              <p className="text-center text-xl md:text-3xl"> I'm a computer science student currently working as an intern at Repl.it! </p>
+              <p className="text-center text-xl md:text-3xl mb-4"> I enjoy messing around with all sorts of cool tech from compilers and AI, to building out awesome looking applications. Feel free to check out my socials or my resume below to see what I can do for you 😊 </p>
+              <div className="w-full flex items-center justify-center">
+                <div className="mx-5 blue-hue cursor-pointer">
+                  <a href="https://www.linkedin.com/in/victor-yuefeng-wang/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <Image src="/images/linkedin-logo.png" height={50} width={50} alt="linkedin" />
+                  </a>
+                </div>
+                <div className="mx-5 blue-hue cursor-pointer">
+                  <a href="https://github.com/vdoubleu"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <Image src="/images/github-sign.png" height={50} width={50} alt="github" />
+                  </a>
+                </div>
+                <div className="mx-5 blue-hue cursor-pointer">
+                  <a href="Victor-Wang.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <Image src="/images/document.png" height={50} width={50} alt="resume" />
+                  </a>
+                </div>
+              </div>
+            </div>  
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <div className="">
+              <p className="text-center text-xl md:text-3xl mb-4">
+                In my free time, I like to make music, speed cube, watch anime, mountain bike, and mess around with cool tech.
+              </p>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <p className="text-center text-xl md:text-3xl mb-4">
+                If you think I'd be a good fit, please reach out! I'm always looking for new opportunities!
+              </p>
+
+              <div className="flex justify-center">
+                <a href="mailto:victorwang2001@gmail.com" rel="noopener noreferrer" className="rounded-xl  bg-[#4BA3E5] px-5 py-2 text-white text-3xl hover:bg-blue-500"> Contact Me </a>
+              </div>
+            </div>
+          </div>
+          <div className="triangle-bottom absolute bottom-0 "></div>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    {/*<div className="left-0 relative triangle-bottom triangle-bottom-height"></div>*/}
+    </>
+  );
 }
